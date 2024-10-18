5473782430ff4a01cc288f3b1713a63ff99dde81
Sebastian Pietras <me@spietras.dev>
2024-10-18 20:32:46 +0200

    Fetched template changes

--- tests/test_test.py
+++ tests/test_test.py
@@ -17,10 +17,10 @@ def data() -> dict[str, str]:
         "packagename": "foo",
         "importname": "foo",
         "description": "Example package",
-        "reponame": "package-foo",
-        "repourl": "https://github.com/radio-aktywne/package-foo",
+        "reponame": "foo",
+        "repourl": "https://github.com/radio-aktywne/foo",
         "docs": "false",
-        "docsurl": "https://radio-aktywne.github.io/package-foo",
+        "docsurl": "https://radio-aktywne.github.io/foo",
         "releases": "false",
     }
 
